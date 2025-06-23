353483ca32091c6f4b52613e2b3414e9b61bc2c7
suman-ops-git <sumanghosh613@gmail.com>
2025-06-23 15:15:23 +0530

    n

--- src/core/feature-components/feedback/feedback.component.ts
+++ src/core/feature-components/feedback/feedback.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Feedback } from 'src/core/model/feedback';
 import { GetApiDataService } from 'src/core/shared-service/get-api-data.service';
 
 @Component({
@@ -10,25 +11,23 @@ export class FeedbackComponent implements OnInit, OnDestroy {
 
   constructor(private getApiDataService: GetApiDataService) { }
 
-  public feedbackList: { comment: string; name: string; date?: string; rating?: number }[] = [];
+  public feedbackList: Array<Feedback> = [];
 
   ngOnInit(): void {
-    this.getApiDataService.getApiData('json/feedbackList.json').subscribe(
-      response => {
+    this.getApiDataService.getApiData('json/feedbackList.json')
+      .subscribe(response => {
         this.feedbackList = response.data || [];
       });
   }
-
-  ngOnDestroy(): void {
-
-  }
-
-  getFeedbackGroup(index: number): { comment: string; name: string; date?: string; rating?: number }[] {
+  
+  getFeedbackGroup(index: number): Array<Feedback> {
     // Show 3 cards per slide, sliding by 3
     const groupSize = 3;
     if (!this.feedbackList.length) return [];
     const start = index * groupSize;
     return this.feedbackList.slice(start, start + groupSize);
   }
+
+  ngOnDestroy(): void {}
 
 }
